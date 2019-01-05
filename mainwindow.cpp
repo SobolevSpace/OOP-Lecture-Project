04325709b05377e74bd200ad0b8888b5a6fbc6a9
SobolevSpace <45968526+SobolevSpace@users.noreply.github.com>
2019-01-05 10:05:30 +0800

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,34 +1,68 @@
-#include "mainwindow.h"
+ï»¿#include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QPainter>
-#include "ImageConverter.h"
-#include "ImageProcess.h"
+#include <QLabel>
+#include <QString>
+#include <string>
+//#include <ImageProcess.h>
 
+//ImageProcess newProcessor;
 MainWindow::MainWindow(QWidget *parent) :
-	QMainWindow(parent),
-	ui(new Ui::MainWindow)
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
-	//ÕâÊÇÒ»¸öÀý³Ì£¬Õ¹Ê¾ÁËGUI¶ËÃ»ÓÐ°üº¬OpenCVÍ·ÎÄ¼þÇé¿öÏÂµÄÊ¹ÓÃ·½·¨¡£
-	//Çë×¢ÒâimageÊÇ±¾µØ±äÁ¿£¬Òò´ËÐèÒªÊ¹ÓÃQImageCopyToMat½øÐÐÉî¸´ÖÆ
-	//·ñÔò¸Ãº¯Êý½áÊøÊ±£¬imageµÄdata¼´±»Îö¹¹¡£
-	//ÎªÊ²Ã´²»ÊÔÊÔ°ÑQImageCopyToMat¸Ä³ÉQImageToMat»á·¢ÉúÊ²Ã´ÄØ£¿
-	auto image = QImage("2chtex.png");
-	Image = ImageConverter::MatToQImage(
-		ImageProcess::generate3ChannelsNormalTexture(
-			ImageConverter::QImageCopyToMat(image)));
-	ui->setupUi(this);
+    ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
-	delete ui;
+    delete ui;
+}
+
+void MainWindow::on_addLayer_clicked() {
+    static int k = 1;
+    QString title;
+    title.setNum(k);//int to QString
+    QByteArray ba = title.toLatin1();
+    const char* ch = ba.data();//QString to char*
+
+    Layer L;//åŽç«¯éœ€è¦åˆ›å»ºä¸€ä¸ªç©ºçš„Layer,idä¸ºk
+
+    addLayerAsBottom(L);
+    k++;
+    ui->toolBox->addItem(new QLabel(ch), tr(ch));
+}
+
+void MainWindow::on_layerUp_clicked() {
+
 }
 
 void MainWindow::paintEvent(QPaintEvent *)
 {
-	QPainter painter(this);
-	painter.drawImage(QRect{0,0,this->width(),this->height()},Image);
-
+//	QPainter painter(this);
+//	for (auto it = newProcessor.Layers.rbegin(); it != newProcessor.Layers.rend(); ++it)
+//	{
+//		QPoint topLeft(it->getTopLeftPoint().first, it->getTopLeftPoint().second);
+//		QPoint bottomRight(it->getBottomRightPoint().first, it->getBottomRightPoint().second);
+//		if (bottomRight.x() == 0 && bottomRight.y() == 0)
+//		{
+//			bottomRight.setX(this->width());
+//			bottomRight.setY(this->height());
+//		}
+//
+//		QRect drawArea(topLeft, bottomRight);
+//      if (it->checkProperty(ImageProcess::DRAW)) {
+//          painter.drawImage(drawArea, ImageConverter::MatToQImage(it->getMat()));
+//      }
+//	}
 }
 
-
+void MainWindow::on_deleteLayer_clicked()
+{
+    int index = ui->toolBox->currentIndex();
+    QString dt = ui->toolBox->itemText(index);
+    if (index != -1)
+    {
+        deleteLayer(dt.toInt());
+        ui->toolBox->removeItem(index);
+    }
+}
